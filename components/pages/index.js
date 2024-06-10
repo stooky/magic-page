9b853ffa16ae71fad756b7859d1dfbb0b07476d3
stooky <chris@fossenier.com>
2024-06-10 11:06:30 -0600

    added pages directory

---
+++ components/pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import Form from '../components/Form';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to My App</h1>
+            <Form />
+        </div>
+    );
+};
+
+export default Home;
