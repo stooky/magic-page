28036b68c6f44ea2044682c40bdcdc5fc261fd18
stooky <chris@fossenier.com>
2024-06-12 14:00:08 -0600

    pathing

--- components/Form.js
+++ components/Form.js
@@ -2,7 +2,7 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import { callZapierWebhook } from '../utils/zapier';
+import { callZapierWebhook } from '../components/utils/zapier';
 
 const jokes = [
     // Your jokes array
