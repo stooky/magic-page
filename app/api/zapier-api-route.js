989e510c0b4a406c97d3fe83247c1f2cfd470405
stooky <chris@fossenier.com>
2024-06-12 15:06:37 -0600

    update to zapier function

--- app/api/zapier-api-route.js
+++ app/api/zapier-api-route.js
@@ -1,12 +1,15 @@
-// components/utils/api/zapier.js
+// app/api/zapier-api-route.js
 import axios from 'axios';
 
 export default async function handler(req, res) {
+    console.log('API route hit'); // Log to check if route is hit
     if (req.method !== 'POST') {
+        console.log('Invalid method');
         return res.status(405).json({ message: 'Method not allowed' });
     }
 
     const { email, website } = req.body;
+    console.log('Request body:', req.body); // Log the request body
 
     const webhookUrl = 'https://hooks.zapier.com/hooks/catch/15076287/3vv4bs9/'; // Replace with your Zapier webhook URL
 
@@ -17,6 +20,7 @@ export default async function handler(req, res) {
             }
         });
 
+        console.log('Webhook response:', response.data); // Log the webhook response
         return res.status(200).json(response.data);
     } catch (error) {
         console.error('Error calling Zapier webhook:', error.response ? error.response.data : error.message);
