31eb4d8124a443e59d0a8f71008d25e18f878ffa
stooky <chris@fossenier.com>
2024-06-12 16:32:52 -0600

    comma or something

--- app/api/zapier-api-route.js
+++ app/api/zapier-api-route.js
@@ -16,8 +16,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             { email, website },
             {
                 headers: {
-                    'Content-Type': 'application/json',
-                },
+                    'Content-Type': 'application/json'
+                }
             }
         );
 
