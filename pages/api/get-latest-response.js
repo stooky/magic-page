ac384be255235270e2b4bd51102b97035de6638b
stooky <chris@fossenier.com>
2024-06-13 15:38:49 -0600

    Add console logging to get-latest-repsonse

--- pages/api/get-latest-response.js
+++ pages/api/get-latest-response.js
@@ -1,12 +1,24 @@
 import { getLatestZapierResponse } from './zapier-callback';
 
 export default async function handler(req, res) {
+    console.log('get-latest-response API called:');
+    console.log('Method:', req.method);
+
     if (req.method !== 'GET') {
+        console.log('Method not allowed:', req.method);
         return res.status(405).json({ message: 'Method not allowed' });
     }
 
     try {
+        console.log('Fetching the latest Zapier response...');
         const response = getLatestZapierResponse();
+        
+        if (response) {
+            console.log('Latest response found:', JSON.stringify(response, null, 2));
+        } else {
+            console.log('No latest response found.');
+        }
+
         return res.status(200).json({ response });
     } catch (error) {
         console.error('Error getting latest response:', error);
