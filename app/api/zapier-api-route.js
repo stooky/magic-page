ef2008c6b2076c7af3081bdaf856d13c0ac7e441
stooky <chris@fossenier.com>
2024-06-13 06:28:26 -0600

    trying to get around a linting error

--- app/api/zapier-api-route.js
+++ app/api/zapier-api-route.js
@@ -1,29 +1,3 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') {
-        return res.status(405).json({ message: 'Method not allowed' });
-    }
-
-    const { email, website } = req.body;
-
-    const webhookUrl = 'https://hooks.zapier.com/hooks/catch/15076287/3vv4bs9/'; // Replace with your Zapier webhook URL
-
-    try {
-        const response = await axios.post(
-            webhookUrl,
-            { email, website },
-            {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-
-        return res.status(200).json(response.data);
-    } catch (error) {
-        console.error('Error calling Zapier webhook:', error.response ? error.response.data : error.message);
-        return res.status(500).json({ message: 'Failed to call Zapier webhook' });
-    }
-}
