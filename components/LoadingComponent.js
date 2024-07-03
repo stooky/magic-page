d4bf12326c9fd4ca7e5ab6700f8d3c93d08023e7
stooky <chris@fossenier.com>
2024-07-03 11:14:59 -0600

    forgot loading component

---
+++ components/LoadingComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingComponent = ({ website }) => (
+    <div className="building-message">
+        Building AI Employee for {website}
+    </div>
+);
+
+export default LoadingComponent;
