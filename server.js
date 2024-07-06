d8780d6984885dbf602f9f8c3d609d67ecf38057
stooky <chris@fossenier.com>
2024-07-06 16:57:36 -0600

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const { createServer } = require('https');
+const http = require('http');
 const { parse } = require('url');
 const next = require('next');
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -14,11 +17,21 @@ const httpsOptions = {
 };
 
 app.prepare().then(() => {
+    const server = express();
+
     createServer(httpsOptions, (req, res) => {
         const parsedUrl = parse(req.url, true);
         handle(req, res, parsedUrl);
     }).listen(443, err => {
         if (err) throw err;
         console.log('> Ready on https://crkid.com:443');
+    });
+
+    // Create HTTP server and redirect all requests to HTTPS
+    http.createServer((req, res) => {
+        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+        res.end();
+    }).listen(80, () => {
+        console.log('HTTP server running on port 80 and redirecting to HTTPS');
     });
 });
