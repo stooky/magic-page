cb47f84631e955ea9c60d6056c7b899782b754f9
stooky <chris@fossenier.com>
2024-08-21 12:50:46 -0600

    What up!!!

--- pages/index.js
+++ pages/index.js
@@ -254,7 +254,7 @@ const MainContainer = () => {
     return (
         <div className="full-screen-container">
             {isLoading ? (
-                <LoadingComponent />  // Render the LoadingComponent when isLoading is true
+                <LoadingComponent />  {/* Render the LoadingComponent when isLoading is true */}
             ) : (
                 <div className="centered-content">
                     <FormComponent onSubmit={handleSubmit} />
@@ -291,7 +291,8 @@ const MainContainer = () => {
                 }
             `}</style>
         </div>
-    );    
+    );
+       
     
     
 };    
