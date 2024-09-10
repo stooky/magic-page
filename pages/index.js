1384f685cfff3cce9b7b715bb98b266786887b9a
stooky <chris@fossenier.com>
2024-09-10 13:46:21 -0600

    Remove two display files not required now.

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,6 @@ import FormComponent from '../components/FormComponent';
 import LoadingComponent from '../components/LoadingComponent';
 import ScanningComponent from '../components/ScanningComponent';
 import Valhallah from '../components/Valhallah.js';
-import StaticMarketingComponent from '../components/StaticMarketingComponent';
-import InfoDisplayComponent from '../components/InfoDisplayComponent';
 import axios from 'axios';
 
 
