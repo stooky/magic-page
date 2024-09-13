1ae635dce6e308091c4d5b55825b89eabd7a5e1c
stooky <chris@fossenier.com>
2024-09-13 14:12:58 -0600

    Adding screenshotUrl to db insert

--- pages/api/dbInsertVisitor.js
+++ pages/api/dbInsertVisitor.js
@@ -3,14 +3,14 @@ const pool = require('../../components/utils/database');
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-        const { sessionID, email, website, companyName, myListingUrl } = req.body;
+        const { sessionID, email, website, companyName, myListingUrl, screenshotUrl } = req.body;
 
         try {
             const query = `
-                INSERT INTO WebsiteVisitors (sessionID, email, website, companyName, myListingUrl)
-                VALUES ($1, $2, $3, $4, $5) RETURNING *;
+                INSERT INTO WebsiteVisitors (sessionID, email, website, companyName, myListingUrl, screenshotUrl)
+                VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;
             `;
-            const values = [sessionID, email, website, companyName, myListingUrl];
+            const values = [sessionID, email, website, companyName, myListingUrl, screenshotUrl];
             const result = await pool.query(query, values);
 
             res.status(200).json({ message: 'Data inserted', data: result.rows[0] });
